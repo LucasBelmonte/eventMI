08dea129257ba7f34453502608d9d20729eaf8b9
Lucas <toshiba.g37@gmail.com>
2023-02-14 19:58:18 -0300

    atualização com tela login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,26 +12,26 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+        <div class="login-box">
 
-            <div>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+
+              <div class="user-box">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
                 <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="mt-4">
+              </div>
+              <div class="user-box">
                 <x-jet-label for="password" value="{{ __('Senha') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
 
-            <div class="block mt-4">
+              </div>
+              <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Lembre de mim') }}</span>
                 </label>
             </div>
-
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
@@ -43,6 +43,13 @@
                     {{ __('Log in') }}
                 </x-jet-button>
             </div>
-        </form>
+              <center>
+              <a href="#">
+                     SEND
+                 <span></span>
+              </a></center>
+            </form>
+          </div>
+
     </x-jet-authentication-card>
 </x-guest-layout>
